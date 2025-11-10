571a96448f79807c42b74fef0afbc47f7026515c
Thabiso67 <160724052+Thabiso67@users.noreply.github.com>
2025-11-10 13:15:40 +0200

    Add files via upload

---
+++ PROG6112ExamQ1/src/main/java/com/mycompany/prog6112examq1/IProductSales.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package com.mycompany.prog6112examq1;
+
+/**
+ *
+ * @author lab_services_student
+ */
+public interface IProductSales {
+    
+    int TotalSales(int[][]productSales);
+    double AverageSales(int[][]productSales);
+    int MaxSale(int[][] productSales);
+}
